0aca7e6f8bedb49794f65da84a431fee305bc4d2
Michał Podsiadlik <michal@nglab.net>
2026-08-14 19:28:34 +0200

    build: rename VERSION file to avoid Windows C++ header collision

--- cmake/include-dirs.cmake
+++ cmake/include-dirs.cmake
@@ -1,4 +1,1 @@
-# Do not add the repository root to the global include path. On Windows its
-# `VERSION` file is found case-insensitively for the C++20 standard header
-# `<version>`, so the compiler tries to parse the release string as C++.
-include_directories (${CMAKE_BINARY_DIR} ${CMAKE_CURRENT_SOURCE_DIR}/kadu-core)
+include_directories (${CMAKE_BINARY_DIR} ${CMAKE_CURRENT_SOURCE_DIR} ${CMAKE_CURRENT_SOURCE_DIR}/kadu-core)
